1b94fc718db199988dd2f635135c279c557a7ad7
Mbaku Starcrush <j.c.nolan9@gmail.com>
2022-10-05 21:32:44 -0700

    Made change to have newReuest explicity be stored in memory and added explanatory comments on storage vs memory

--- contracts/crowdfunding.sol
+++ contracts/crowdfunding.sol
@@ -32,12 +32,21 @@ contract Campaign {
         public restricted {
             
             //have to provide values for all the fields declared in the struct definition up top
-            Request newRequest = Request({
+            Request memory newRequest = Request({
                 description: description,
                 value: value,
                 recipient: recipient,
                 complete: false
             });
+            /*
+                have to use 'memory' when creating the Request because the only variables that can use
+                'storage' are the instance variables. As in the only variables  in storage are 
+                the instance variables declared above. We cannot point 'newRequest' to any of the
+                instance variables above, not even 'requests' because none of them are a single
+                Request.
+
+                Local variables like those inside a function are also by default 'memory' I think 
+            */
 
             /*
                 alternative syntax for instantiating a struct 
@@ -65,4 +74,40 @@ contract Campaign {
         - like a computer's RAM
         - data in RAM doesn't persist when restart your computer 
         - arguments to a function are in "memory"
+
+    How Solidity stores variables 
+
+        Storage
+            int[] public numbers;
+            numbers.push(20);
+            numbers.push(32);
+
+            int storage myArray = numbers;
+            //myArray points at the exact storage location/data structure that 'numbers' points at
+            //altering myArray will also alter 'numbers'
+
+        Memory
+            int[] public numbers;
+            numbers.push(20);
+            numbers.push(32);
+
+            int memory myArray = numbers;
+            //myArray is a copy of numbers but myArray and 'numbers' are two separate objects
+            //changing myArray will not change 'numbers'
+
+            When you pass an argument to a function, if that argument is an already existing varaible,
+            then you are passing a copy of that variable to the function. In fact, you are always passing 
+            a copy. The function uses a copy of the argument you passed  
+
+            function changeArray(int[] memory myArray) private {
+                ...
+            }
+
+            is the same as 
+
+            function changeArray(int[] myArray) private {
+                ...
+            }
+
+
 */
