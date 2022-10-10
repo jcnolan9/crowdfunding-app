8a435126ca464464d11562bb63ed8d5da4434e33
Mbaku Starcrush <j.c.nolan9@gmail.com>
2022-10-09 22:41:49 -0700

    using Semantic UI React to create components,  using Next.js Routes to create routes to pages, setting up server.js to use Next.js routes

--- pages/show.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default () => {
-    return (
-        <div>Welcome to the SHOW campaign page!</div>
-    )
-}
---
+++ routes.js
@@ -0,0 +1,11 @@
+const routes = require('next-routes')()
+
+routes
+    .add('/campaigns/new', 'campaigns/new') 
+    //have to have this first cause '/campaigns/new' also matches route pattern below
+    .add('/campaigns/:address', '/campaigns/show') 
+//the ':' indicates this part of the URL is going to be a wildcard
+//'address' is a variable name that you can use to know what page the user is trying to get to
+//second argument to 'add' function is which route you want user to go to if they type something looks like the first argument 
+
+module.exports = routes
